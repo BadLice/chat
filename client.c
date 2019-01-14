9a6ad174c534892428ae6f2aea5d904a75ca667e
Alex Licata <alerandom99@gmail.com>
2019-01-14 22:51:38 +0100

    added accounts with username and passowrds

--- client.c
+++ client.c
@@ -12,14 +12,12 @@ void* read(void* args)
 		int recv_size;
 		char server_reply[2000];
 
-		// printf("Listening...\n");
 		if((recv_size = recv(*s,server_reply,2000,0))==SOCKET_ERROR)
 		{
 			printf("Server disconnected.\n");
 			STOP = 1;
 			return (void*)1;
 		}
-		// printf("Recived\n");
 		server_reply[recv_size]='\0';
 		printf("%s\n",server_reply );
 	}
@@ -29,12 +27,10 @@ void* write(void* args)
 {
 	while(1)
 	{
-		
 		SOCKET* s = (SOCKET *) args;
 		char message[30];
 		scanf("%[^\n]%*c",message);
 		fflush(stdin);
-		// printf("Sending %s\n",message );
 
 		if(send(*s,message,strlen(message),0)<0)
 		{
@@ -42,8 +38,40 @@ void* write(void* args)
 			STOP = 1;
 			return (void*)1;
 		}
-		// printf("sent.\n");		
 	}
+}
+
+int validateUsername(char* username)
+{
+	int i;
+	for(i=0;i<strlen(username);i++)
+	{
+		if(username[i]=='*')
+			return 0;
+	}
+
+	return 1;
+}
+
+int setCommand(char* commandServer, char cmd)
+{
+	char login[15]="\\LOG_IN*";
+	char singin[15]="\\SGN_IN*";
+
+	switch(cmd)
+	{
+		case '1':
+			strcpy(commandServer,singin);
+		break;
+		
+		case '2':
+			strcpy(commandServer,login);
+		break;
+
+		default: return 0; break;
+	}
+
+	return 1;
 }
 
 
@@ -69,6 +97,8 @@ int main(int argc, char const *argv[])
 	scanf("%s",IP);
 	fflush(stdin);
 
+	// strcpy(IP,"127.0.0.1");
+
 	struct sockaddr_in server;
 	server.sin_addr.s_addr = inet_addr(IP);
 	server.sin_family = AF_INET;
@@ -81,37 +111,71 @@ int main(int argc, char const *argv[])
 	}
 	printf("Connected!\n");
 
-	int validate;
+	char command;
+	char *commandServer=calloc(15,sizeof(char));
 	do
 	{
-		printf("Insert username: ");
-		char message[30],valid[30];
-		validate = 0;
-		scanf("%s",message);
+		printf("Choose:\n");
+		printf("1 - Sing in\n");
+		printf("2 - Log in\n");
+
+		scanf("%c",&command);
 		fflush(stdin);
+
+		int validate;
+		char username[30], password[1000], valid[30];
+		
+		validate = 0;
 		int recv_size;
 
-
-		if(send(s,message,strlen(message),0)<0)
+		do
 		{
-			printf("Cannot send username!\n");
-			return 1;
-		}
+			if(setCommand(commandServer,command))
+			{
+				do
+				{
+					printf("Insert username: ");
+					scanf("%s",username);
+					fflush(stdin);
+				} while(!validateUsername(username));
 
-		if((recv_size = recv(s,valid,30,0)) != SOCKET_ERROR)
-		{
-			valid[recv_size]='\0';
-			if(!strcmp(valid,"\\USRNM_N_VLD"))
-				validate=0;
+				printf("Insert password: ");
+				scanf("%s",password);
+				fflush(stdin);
+
+				strcat(commandServer,username);
+
+				if(send(s,commandServer,strlen(commandServer),0)<0)
+				{
+					printf("Cannot send username!\n");
+					return 1;
+				}
+
+				if(send(s,password,strlen(password),0)<0)
+				{
+					printf("Cannot send username!\n");
+					return 1;
+				}
+
+				if((recv_size = recv(s,valid,30,0)) != SOCKET_ERROR)
+				{
+					valid[recv_size]='\0';
+					if(!strcmp(valid,"\\USRNM_N_VLD"))
+						validate=0;
+					else
+					if(!strcmp(valid,"\\USRNM_VLD"))
+						validate = 1;
+				}
+
+				if(!validate)
+					printf("Username already used or invalid.\n");
+			}
 			else
-			if(!strcmp(valid,"\\USRNM_VLD"))
-				validate = 1;
-		}
+				break;
+		
+		} while(!validate);
 
-		if(!validate)
-			printf("Username already used or invalid.\n");
-	
-	} while(!validate);
+	} while(command !='1' && command!='2');	
 	
 	printf("Validated\n");
 	SOCKET *s1 = malloc(sizeof(SOCKET));
@@ -123,7 +187,6 @@ int main(int argc, char const *argv[])
 	pthread_t readTh; 
 	// printf("Reading Thread Started\n"); 
 	pthread_create(&readTh, NULL, read, (void*)s1);
-
 
 	pthread_t writeTh; 
 	// printf("Writing Thread Started\n"); 
--- server.c
+++ server.c
@@ -1,3 +1,5 @@
+//if (password of x) = (username y) ?
+
 #include "stdio.h"
 #include "winsock2.h"
 #include "string.h"
@@ -13,7 +15,7 @@
 #endif
 
 int toUpdate=-1;
-
+char accountPath[30] = "AccountDB.db";
 pthread_mutex_t lock;
 
 typedef struct
@@ -37,15 +39,111 @@ typedef struct
 
 } updateStr;
 
-int isUsed(char* msg, CLIENT* clients)
+int isOnline(CLIENT *clients,char* username)
 {
 	int i;
 	for(i=0;i<MAX_CLIENTS;i++)
 	{
-		if(!strcmp(clients[i].username,msg))
-			return 1;
+		if(clients[i].valid)
+			if(!strcmp(username,clients[i].username))
+				return 1;
 	}
 	return 0;
+}
+
+int file_exists(const char * filename)
+{
+	FILE *file;
+    if ( file = fopen(filename, "r"))
+    {
+        fclose(file);
+        return 1;
+    }
+    return 0;
+}
+
+void createFirstFile(const char* path)
+{
+	if(!file_exists(path))
+	{
+		FILE* f = fopen(path,"w");
+		fclose(f);
+	}
+}
+
+int strContains(char* str, const char* cont)
+{
+	int i,j;
+	for(i=0;i<strlen(str);i++)
+	{
+		if(str[i]==cont[0])
+		{ 
+			int ok = 1;
+			for(j=0;(i+j)<strlen(str) && j<strlen(cont) &&ok;j++)
+			{
+				if(str[i+j]!=cont[j])
+					ok = 0;
+			}
+
+			if(ok)
+				return 1;
+		}
+	}
+
+	return 0;
+}
+
+
+int isUsedUsername(char* msg,char* path)
+{
+	int ok = 0;
+	char buff[100]="";
+	strcat(buff,"-");
+	strcat(buff,msg);
+
+	FILE* f = fopen(path,"r");
+	char usrnm[33];
+
+	while(!feof(f) && !ok)
+	{
+		if(fgets(usrnm,33,f)!=NULL)
+		{
+			usrnm[strlen(usrnm)-1] = '\0';
+			if(!strcmp(usrnm,buff))
+				ok=1;
+		}
+	}
+
+	fclose(f);
+	return ok;	
+}
+
+char* findPassword(char* username, char* path)
+{
+	FILE *f = fopen(path,"r");
+	char rd[1000] = "";
+	char* psw = malloc(1000*sizeof(char));
+
+	char usrnm[50]="";
+	strcat(usrnm,"-");
+	strcat(usrnm,username);
+	strcat(usrnm,"\n");
+
+	while(!feof(f))
+	{
+		fgets(rd,1000,f);
+		if(!strcmp(rd,usrnm) && !feof(f))
+		{
+			fgets(psw,1000,f);
+			if(!(strlen(psw)<=0 || !strcmp(psw,"\n")))
+			{
+				psw[strlen(psw)-1]='\0';
+				return psw;
+			}
+		}
+	}
+
+	return NULL;
 }
 
 int getSocketIndex(CLIENT *clients)
@@ -73,6 +171,63 @@ CLIENT* initClients()
 	return c;
 }
 
+void loginUser(CLIENT *clients, int currentIndex,char* username)
+{
+	strcpy(clients[currentIndex].username,username);
+	printf("Client %s connected!\n", clients[currentIndex].username);
+	char sz[30] = "\\USRNM_VLD";//username valid	
+	send(clients[currentIndex].socket,sz,strlen(sz),0);
+}
+
+int saveNewUser(char* username, char* passw, char* path)
+{
+	char s1[strlen(username)+1];
+	char s2[strlen(passw)+1];
+
+	s1[0]='\0';
+	s2[0]='\0';
+
+	strcat(s1,"-");
+	strcat(s2,"+");
+	
+	strcat(s1,username);
+	strcat(s2,passw);
+	
+	int noerr = 1;
+	strcat(s1,"\n");
+	strcat(s2,"\n\n");
+
+	FILE *accounts = fopen(path,"a+");
+	if(fputs(s1,accounts)==EOF || fputs(s2,accounts)==EOF)
+		noerr = 0;
+
+	fclose(accounts);
+
+	return noerr;
+}
+
+void sendUsernameError(CLIENT* clients, int currentIndex)
+{
+	printf("Invalid username from client %d, asking again...\n",currentIndex);
+	char sz[30] = "\\USRNM_N_VLD";//username not valid
+	send(clients[currentIndex].socket,sz,strlen(sz),0);
+					
+}
+
+void sendPasswordError(CLIENT* clients, int currentIndex)
+{
+	printf("Password not valid for client %d.\n",currentIndex);
+	char sz[30] = "\\USRNM_N_VLD";//username not valid
+	send(clients[currentIndex].socket,sz,strlen(sz),0);
+					
+}
+
+void sendAlreadyOnlineError(CLIENT* clients, int currentIndex, char* insertedUsername)
+{
+	printf("User \"%s\" is trying to log in from another client!\n",insertedUsername);
+	char sz[30] = "\\USRNM_N_VLD";//username not valid
+	send(clients[currentIndex].socket,sz,strlen(sz),0);
+}
 
 void* updateClient(void* arg)
 {
@@ -83,6 +238,10 @@ void* updateClient(void* arg)
 		updateStr *args = (updateStr*) arg;
 		int recv_size;
 		char msg[1000];
+		char passw[1000];
+
+		FILE* accounts;
+		
 		pthread_mutex_unlock(&lock);
 
 		if((recv_size = recv(args->clients[args->currentIndex].socket,msg,1000,0)) != SOCKET_ERROR) //recieve from current and send to all other clients
@@ -92,24 +251,98 @@ void* updateClient(void* arg)
 
 			if(strlen(args->clients[args->currentIndex].username)==0)
 			{
-				if(strlen(msg)<=0 || strlen(msg)>32  || isUsed(msg,args->clients))
+				//sign in procedure
+				if(strContains(msg,"\\SGN_IN"))
 				{
-					printf("Invalid username from a new connection, asking again...\n");
-					char sz[30] = "\\USRNM_N_VLD";//username not valid
-					send(args->clients[args->currentIndex].socket,sz,strlen(sz),0);
+					char* token = strtok(msg,"*");
+					token = strtok(NULL,"*");
+
+					//validate username
+					if(strlen(token)<=0 || strlen(token)>32  || isUsedUsername(token,accountPath))
+					{
+						sendUsernameError(args->clients,args->currentIndex);
+					}
+					else
+					{
+						//request password
+						//not crypted yet
+						printf("Listening\n");
+						pthread_mutex_unlock(&lock);
+						if((recv_size = recv(args->clients[args->currentIndex].socket,passw,1000,0)) != SOCKET_ERROR)
+						{
+							printf("Checking\n");
+							pthread_mutex_lock(&lock);
+							passw[recv_size]='\0';
+
+							//write password to database
+							if(strlen(passw)!=0)
+							{
+
+								if(!saveNewUser(token,passw,accountPath))
+									printf("Error saving new user!\n");
+								else
+									loginUser(args->clients,args->currentIndex,token);
+							}
+
+						}
+						else
+						{
+							pthread_mutex_lock(&lock);			
+						}
+						printf("Listened\n");
+					}	
 				}
-				else
+				//login procedure
+				if(strContains(msg,"\\LOG_IN"))
 				{
-					strcpy(args->clients[args->currentIndex].username,msg);
-					printf("Client %s connected!\n", args->clients[args->currentIndex].username);
-					char sz[30] = "\\USRNM_VLD";//username valid	
-					send(args->clients[args->currentIndex].socket,sz,strlen(sz),0);
+					char* psw;
+					char recpass[1000];
+					char* token = strtok(msg,"*");
+					token = strtok(NULL,"*");//contains right username
+					if((psw = findPassword(token,accountPath))!=NULL)
+					{
+						pthread_mutex_unlock(&lock);
+						if((recv_size = recv(args->clients[args->currentIndex].socket,recpass,1000,0)) != SOCKET_ERROR)
+						{
+							pthread_mutex_lock(&lock);
+							recpass[recv_size]='\0';
 
+							//password valid, login
+							if(!isOnline(args->clients,token))
+							{
+								if(!strcmp(psw,recpass))
+								{	
+									loginUser(args->clients,args->currentIndex,token);
+								}
+								else
+								{
+									sendPasswordError(args->clients,args->currentIndex);
+								}
+							}
+							else
+							{
+								sendAlreadyOnlineError(args->clients,args->currentIndex,token);
+							}
+							
+						}
+						else
+						{
+							pthread_mutex_lock(&lock);
+
+							printf("Cannot recieve password\n");
+						}
+
+					}
+					else
+					{
+						sendUsernameError(args->clients,args->currentIndex);
+					}
 				}
-
+				
 			}
 			else
 			{
+				//sned normal message
 				int i;
 				for(i=0;i<MAX_CLIENTS;i++)
 				{
@@ -130,6 +363,8 @@ void* updateClient(void* arg)
 		}
 		else
 		{
+			//client disconnected
+			pthread_mutex_lock(&lock);
 			printf("Client %s disconnected.\n",args->clients[args->currentIndex].username);
 			args->clients[args->currentIndex].username[0]='\0';
 			args->clients[args->currentIndex].socket = INVALID_SOCKET;
@@ -138,8 +373,8 @@ void* updateClient(void* arg)
 			pthread_mutex_unlock(&lock);
 			return (void*) 1;
 		}
+
 		pthread_mutex_unlock(&lock);
-		
 	}
 }
 
@@ -206,6 +441,8 @@ int main(int argc, char const *argv[])
 	}
 
 	printf("Socket bound.\n");
+
+	createFirstFile(accountPath);
 
 	sniffStr arg;
 	arg.clients = clients;
